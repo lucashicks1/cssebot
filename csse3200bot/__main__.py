8fff89c07f30866c61e5d8fb829bb6bc9f38077e
lucashicks1 <lucasjhicks1@gmail.com>
2025-07-26 00:28:16 +1000

    nvim to the rescue

--- csse3200bot/__main__.py
+++ csse3200bot/__main__.py
@@ -3,14 +3,21 @@
 import asyncio
 import logging
 
+import discord
+
+from csse3200bot.config import CONFIG
+
 
 async def main() -> None:
+    """Main."""
     logging.basicConfig(level=logging.INFO)
 
-    # _ = discord.Intents.default()
+    intents = discord.Intents.default()
     # # Not sure what intents we need atm
 
-    print("STARTING UP")
+    client = discord.Client(intents=intents)
+
+    client.run(CONFIG.discord_bot_token)
 
 
 asyncio.run(main())
