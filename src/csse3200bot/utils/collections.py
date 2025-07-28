3bce8dabf866d8950e1cefa7be9e3b2ebb8fa713
lucashicks1 <lucasjhicks1@gmail.com>
2025-07-28 18:01:04 +1000

    new: adds cache utils.

---
+++ src/csse3200bot/utils/collections.py
@@ -0,0 +1,80 @@
+"""Collection Utils."""
+
+from abc import ABC
+from collections.abc import Awaitable, Callable
+from time import time
+
+DEFAULT_CACHE_TTL = 300
+
+
+class _BaseCache[T, S](ABC):
+    """Abstract base class for cache."""
+
+    _ttl: int
+    _cache: dict[T, tuple[float, S]]
+
+    def __init__(self, ttl: int = DEFAULT_CACHE_TTL) -> None:
+        """Abstract cache."""
+        self._ttl = ttl
+        self._cache = {}
+
+    def _is_valid(self, timestamp: float) -> bool:
+        return (time() - timestamp) < self._ttl
+
+    def clear(self) -> None:
+        """Clear the entire cache."""
+        self._cache.clear()
+
+
+class SyncCache[T, S](_BaseCache[T, S]):
+    """Synchronous cache."""
+
+    _fetch_callback: Callable[[T], S | None]
+
+    def __init__(self, fetch_callback: Callable[[T], S | None], ttl: int = DEFAULT_CACHE_TTL) -> None:
+        """Construct a synchronous cache.
+
+        Args:
+            fetch_callback (Callable[[T], S  |  None]): callable to fetch data
+            ttl (int, optional): basic time to live (in seconds). Defaults to DEFAULT_CACHE_TTL.
+        """
+        super().__init__(ttl)
+        self._fetch_callback = fetch_callback
+
+    def get(self, key: T) -> S | None:
+        """Get something from the cache."""
+        cached = self._cache.get(key)
+        if cached and self._is_valid(cached[0]):
+            return cached[1]
+
+        result = self._fetch_callback(key)
+        if result is not None:
+            self._cache[key] = (time(), result)
+        return result
+
+
+class AsyncCache[T, S](_BaseCache[T, S]):
+    """Asynchronous cache."""
+
+    _fetch_callback: Callable[[T], Awaitable[S | None]]
+
+    def __init__(self, fetch_callback: Callable[[T], Awaitable[S | None]], ttl: int = DEFAULT_CACHE_TTL) -> None:
+        """Construct a asynchronous cache.
+
+        Args:
+            fetch_callback (Callable[[T], Awaitable[S  |  None]]): async callable to fetch data
+            ttl (int, optional): basic time to live (in seconds). Defaults to DEFAULT_CACHE_TTL.
+        """
+        super().__init__(ttl)
+        self._fetch_callback = fetch_callback
+
+    async def get(self, key: T) -> S | None:
+        """Get something from the cache."""
+        cached = self._cache.get(key)
+        if cached and self._is_valid(cached[0]):
+            return cached[1]
+
+        result = await self._fetch_callback(key)
+        if result is not None:
+            self._cache[key] = (time(), result)
+        return result
